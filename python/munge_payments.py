6c6fef535e825cc8dd21b3ce86dddb8845636420
river <river@DELL-PC.(none)>
2013-08-09 22:45:03 +1000

    Updated index.html to remove some of the old song related cruft, and make munge_payments iterate through 3 different parties.

--- python/munge_payments.py
+++ python/munge_payments.py
@@ -1,33 +1,52 @@
+#!/usr/bin/env python
+
 from collections import defaultdict
+from collections import namedtuple
 import xml.etree.ElementTree as ET
 import json
 import locale
 
-tree = ET.parse('../AnalysisPartyGroupTheGreens.xml')
-root = tree.getroot()
+def xmltojson(filename, name, party_id):
+  tree = ET.parse(filename)
+  root = tree.getroot()
 
-payments = defaultdict(int)
+  payments = defaultdict(int)
 
-locale.setlocale( locale.LC_ALL, '' )
+  locale.setlocale( locale.LC_ALL, '' )
 
-for node in root:
-    payments[node.find('PayerClientNm').text] += int(node.find('AmountReceived').text)
+  for node in root:
+      payments[node.find('PayerClientNm').text] += int(node.find('AmountReceived').text)
 
-totalpayments = sum(payments.values())
-nodes = [{'name':"Australian Greens Party", 
-    'amount':locale.currency(totalpayments, grouping=True), 
-    'playcount':totalpayments, 'id':'australian_greens_party'}]
-links = []
-for key, val in payments.iteritems():
-   node = {}
-   id = key.replace(' ', '_').lower()
-   node['match'] = float(val) / totalpayments
-   node['name'] = key
-   node['amount'] = locale.currency(val, grouping=True)
-   node['playcount'] = val
-   node['id'] = id
-   nodes.append(node)
-   links.append({'source': "australian_greens_party", 'target': id})
-   
-print json.dumps({'nodes': nodes, 'links': links}, indent=2)
+  totalpayments = sum(payments.values())
+  nodes = [{'name':name, 
+      'amount':locale.currency(totalpayments, grouping=True), 
+      'playcount':totalpayments, 'id':party_id}]
+  links = []
+  for key, val in payments.iteritems():
+     node = {}
+     id = key.replace(' ', '_').lower()
+     node['match'] = float(val) / totalpayments
+     node['name'] = key
+     node['amount'] = locale.currency(val, grouping=True)
+     node['playcount'] = val
+     node['id'] = id
+     nodes.append(node)
+     links.append({'source': id, 'target': party_id})
+     
+  fp = open(party_id + '.json', 'w')
+  json.dump({'nodes': nodes, 'links': links}, fp, indent=2)
+
+Party = namedtuple('Party', ['filename', 'name', 'party_id'])
+
+parties = [
+  Party("AnalysisPartyGroupAustralianLaborParty.xml", 
+      "Australian Labor Party", "australian_labor_party"),
+  Party("AnalysisPartyGroupTheGreens.xml", 
+      "Australian Greens Party", "australian_greens_party"),
+  Party("AnalysisPartyGroupLiberal.xml", 
+      "Liberal Party", "liberal_party")
+]
+
+for party in parties:
+  xmltojson(*party)
 
