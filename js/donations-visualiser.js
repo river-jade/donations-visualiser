e0e4272c39ea9cbc94fe4a31a218e32708356689
themrb <themrb4@gmail.com>
2016-06-29 21:19:13 +1000

    fixed window resize starting layout tick before data is loaded

--- js/donations-visualiser.js
+++ js/donations-visualiser.js
@@ -20,7 +20,8 @@ var party_map = {},
     years = [],
     receipt_types,
     clickedNode = null,
-    oldYear = -1;
+    oldYear = -1,
+    data_loaded = false;
 
 // http://bootstraptour.com/api/
 var tour = new Tour({
@@ -156,7 +157,7 @@ function resizeWindow() {
     fireCoolingHandler = true;
 
     force.size([width, height]);
-    force.start();
+    if (data_loaded) force.start();
 }
 
 d3.select(w).on("resize", resizeWindow);
@@ -256,6 +257,7 @@ var data_request = d3.json("data/all_data.json")
         d3.select("#loading-progress").style("width", "100%");
         $("#loading-modal").modal('hide');
         processData(data);
+        data_loaded = true;
 
         // zoom out a ways initially
         zoomTo(0.5).event(container);
