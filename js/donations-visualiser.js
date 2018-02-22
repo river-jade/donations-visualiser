fc9c51bfb93a5b55e3a4de107b13ccdd7b1d7976
themrb <themrb4@gmail.com>
2018-02-22 21:17:57 +1100

    removed tour

--- js/donations-visualiser.js
+++ js/donations-visualiser.js
@@ -32,77 +32,6 @@ var narrowClient = $(window).width() < 800,
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
 
-
-// http://bootstraptour.com/api/
-var tour = new Tour({
-    backdrop: true,
-    debug: true,
-    delay: {
-        show: 400,
-        hide: 0
-    },
-    steps: [
-        {
-            title: "About this site",
-            content: "This graph represents all of the donations to Australian " +
-               "federal political parties in a given financial year.",
-            element: "#vis .graph .container",
-            placement: "left",
-            backdrop: true,
-        },
-        {
-            title: "About this site",
-            content: "Square nodes represent political parties",
-            element: "path.node.australian-labor-party.party",
-            backdrop: true,
-        },
-        {
-            title: "About this site",
-            content: "Circular nodes represent donors",
-            element: "path.node.westpac-banking-corporation.entity",
-            backdrop: true,
-        },
-        {
-            title: "Details view",
-            content: "Clicking on a node gives details for that entity (eg party or " +
-                "donor), including top donations, and donations over time.",
-            element: "path.node.coalition.party",
-            backdrop: true,
-            placement: "left",
-            onShow: function(tour) {
-                d3Click("path.node.coalition.party");
-                toggleInfoPanel(null, true);
-            }
-        },
-        {
-            title: "Details view",
-            content: "Clicking the name in the details panel opens a Google search for that entity.",
-            element: "#info-panel h3 a",
-            backdrop: true,
-            placement: "left",
-        },
-        {
-            title: "Details view",
-            content: "Hovering over the donor name shows them in the graph. Click on a name for more details.",
-            element: "#info-table",
-            backdrop: true,
-            placement: "left",
-        },
-        {
-            title: "Filter panel",
-            content: "You can filter the results, and show different years, by opening the filter panel.",
-            element: "#filter-toggle",
-            backdrop: true,
-            onShow: function(tour) {
-                toggleFilterPanel(null, true);
-            }
-        },
-    ],
-    afterSetState: function (key, value) {
-        logClick('tour.gettingStarted', key, value);
-    }
-});
-
 var party_logos = [
   {
     name: "Coalition",
@@ -302,9 +231,6 @@ d3.select("#receipt-type-select-clear").on("click", function() { clearSelection(
 d3.select("#clear-search").on("click", clearSearch);
 
 var $gettingStarted = $('#getting-started-modal');
-d3.select('.js-handleStartTour').on('click', function() {
-    if (tour.ended) tour.restart();
-});
 
 // on 'escape' key press, close the info window, and zoomToFit
 $("body").on("keydown", function (event) {
@@ -388,10 +314,6 @@ function coolHandler() {
 
         window.setTimeout(function() {
             if (!isMobile) toggleFilterPanel(null, true);
-            if (firstLoad()) {
-                tour.init();
-                tour.restart();
-            }
         }, 300);
     }
 }
